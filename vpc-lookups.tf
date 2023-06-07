1014a7eba6707beab18e02806624049c2fccd006
Marko <mskender@schubergphilis.com>
2023-06-07 16:07:19 +0200

    init

--- vpc-lookups.tf
+++ vpc-lookups.tf
@@ -1,6 +1,8 @@
 locals {
-    private_subnet_ids = var.lookup_private_subnets ? data.aws_subnets.private[0].ids:[]
-    public_subnet_ids = var.lookup_public_subnets ? data.aws_subnets.public[0].ids:[]
+  private_subnet_ids = var.lookup_private_subnets ? data.aws_subnets.private[0].ids : []
+  public_subnet_ids  = var.lookup_public_subnets ? data.aws_subnets.public[0].ids : []
+
+
 }
 
 data "aws_availability_zones" "az" {
@@ -9,36 +11,36 @@ data "aws_availability_zones" "az" {
 ### VPC
 data "aws_vpc" "vpc" {
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-vpc"
+      "${var.prefix}-vpc"
     ]
   }
 }
 data "aws_subnets" "private" {
-  count = var.lookup_private_subnets ? 1:0
+  count = var.lookup_private_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-private*"
+      "${var.prefix}-private*"
     ]
   }
 }
 
 data "aws_subnets" "public" {
-  count = var.lookup_public_subnets ? 1:0
+  count = var.lookup_public_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-public*"
+      "${var.prefix}-public*"
     ]
   }
 }
@@ -56,82 +58,82 @@ data "aws_subnet" "public" {
 ### SUB BY SUB
 
 data "aws_subnet" "public_a" {
-  count = var.lookup_public_subnets ? 1:0
+  count = var.lookup_public_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-public-1a"
+      "${var.prefix}-public-${split("-", element(data.aws_availability_zones.available.names, 0))[2]}"
     ]
   }
 }
 data "aws_subnet" "public_b" {
-  count = var.lookup_public_subnets ? 1:0
+  count = var.lookup_public_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-public-1b"
+      "${var.prefix}-public-${split("-", element(data.aws_availability_zones.available.names, 1))[2]}"
     ]
   }
 }
 data "aws_subnet" "public_c" {
 
-  count = var.lookup_public_subnets ? 1:0
+  count = var.lookup_public_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-public-1c"
+      "${var.prefix}-public-${split("-", element(data.aws_availability_zones.available.names, 2))[2]}"
     ]
   }
 }
 
 data "aws_subnet" "private_a" {
-  count = var.lookup_private_subnets ? 1:0
+  count = var.lookup_private_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-private-1a"
+      "${var.prefix}-private-${split("-", element(data.aws_availability_zones.available.names, 0))[2]}"
     ]
   }
 }
 data "aws_subnet" "private_b" {
-    count = var.lookup_private_subnets ? 1:0
+  count = var.lookup_private_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-private-1b"
+      "${var.prefix}-private-${split("-", element(data.aws_availability_zones.available.names, 1))[2]}"
     ]
   }
 }
 data "aws_subnet" "private_c" {
-    count = var.lookup_private_subnets ? 1:0
+  count = var.lookup_private_subnets ? 1 : 0
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = [
-        "${var.prefix}-private-1c"
+      "${var.prefix}-private-${split("-", element(data.aws_availability_zones.available.names, 1))[2]}"
     ]
   }
 }
